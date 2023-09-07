fa67ef43384df1b400e1e29e45b0d881c0a97c64
Sakina Mbatha <sakina@natix.io>
2023-09-07 16:47:04 +0200

    feat: added a text field to the modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -29,9 +30,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseModal() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text(
-        'modal bottom sheet',
-      ),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
