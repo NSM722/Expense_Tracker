5564afd8dffd5f0b564d6189167925427ce91a27
Sakina Mbatha <sakina@natix.io>
2023-09-07 13:44:33 +0200

    feat: added expenses model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,22 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+// fixed set of values/custom type
+//the values in the enum are treated as strings
+enum Category { food, travel, leisure, work }
+
 class Expense {
   // constructor function with named parameters
-  Expense({required this.title, required this.amount, required this.date})
-      : id = uuid.v4(); // initializer list generating unique ids
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4(); // initializer list generating unique ids
+
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
