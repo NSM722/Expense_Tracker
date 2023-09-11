1f5bbea7bd29aa2d685274973ef1f987d98cb629
Sakina Mbatha <sakina@natix.io>
2023-09-11 11:25:10 +0200

    style: added dark mode theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 128, 227, 179),
 );
 
+var kDarkModeScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 34, 65, 44),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDarkModeScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkModeScheme.secondaryContainer,
+            margin: const EdgeInsets.all(12),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkModeScheme.primaryContainer,
+              foregroundColor: kDarkModeScheme.onPrimaryContainer,
+            ),
+          )),
       theme: ThemeData(
           useMaterial3: true,
           colorScheme: kColorScheme,
@@ -32,6 +50,7 @@ void main() {
                   fontSize: 16,
                 ),
               )),
+      // themeMode: ThemeMode.system, // control the theme mode which is default
       home: const Expenses(),
     ),
   );
