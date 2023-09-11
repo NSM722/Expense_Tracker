ce9ca971bc9eff8062e633da736b6ce739f01400
Sakina Mbatha <sakina@natix.io>
2023-09-11 10:17:37 +0200

    style: overwriting default theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 219, 127, 157),
       ),
       home: const Expenses(),
     ),
