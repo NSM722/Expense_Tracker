a789b286927f219db60e925fbac104b8efe03ddc
Sakina Mbatha <sakina@natix.io>
2023-09-11 10:44:33 +0200

    style: setting text themes

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,35 @@ import 'package:flutter/material.dart';
 
 // color scheme - it's common for global variables to begin with 'k'
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 128, 227, 179),
+  seedColor: const Color.fromARGB(255, 128, 227, 179),
 );
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(
+      theme: ThemeData(
           useMaterial3: true,
           colorScheme: kColorScheme,
           appBarTheme: const AppBarTheme().copyWith(
             backgroundColor: kColorScheme.onPrimary,
             foregroundColor: kColorScheme.onPrimaryContainer,
-          )),
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.all(12),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14,
+                ),
+              )),
       home: const Expenses(),
     ),
   );
