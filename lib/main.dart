1c7eb1d68d92a006151ab2ae3e86a9a7efc1a879
Sakina Mbatha <sakina@natix.io>
2023-09-11 14:36:06 +0200

    feat: locked orientation mode of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // color scheme - it's common for global variables to begin with 'k'
 var kColorScheme = ColorScheme.fromSeed(
@@ -12,46 +13,51 @@ var kDarkModeScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: kDarkModeScheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkModeScheme.secondaryContainer,
-            margin: const EdgeInsets.all(12),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kDarkModeScheme.primaryContainer,
-              foregroundColor: kDarkModeScheme.onPrimaryContainer,
-            ),
-          )),
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.onPrimary,
-          ),
-          cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: const EdgeInsets.all(12),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: kColorScheme.onSecondaryContainer,
-                  fontSize: 16,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (fn) {
+      runApp(
+        MaterialApp(
+          darkTheme: ThemeData.dark().copyWith(
+              useMaterial3: true,
+              colorScheme: kDarkModeScheme,
+              cardTheme: const CardTheme().copyWith(
+                color: kDarkModeScheme.secondaryContainer,
+                margin: const EdgeInsets.all(12),
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kDarkModeScheme.primaryContainer,
+                  foregroundColor: kDarkModeScheme.onPrimaryContainer,
                 ),
               )),
-      // themeMode: ThemeMode.system, // control the theme mode which is default
-      home: const Expenses(),
-    ),
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: kColorScheme,
+              appBarTheme: const AppBarTheme().copyWith(
+                backgroundColor: kColorScheme.onPrimaryContainer,
+                foregroundColor: kColorScheme.onPrimary,
+              ),
+              cardTheme: const CardTheme().copyWith(
+                color: kColorScheme.secondaryContainer,
+                margin: const EdgeInsets.all(12),
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kColorScheme.primaryContainer,
+                ),
+              ),
+              textTheme: ThemeData().textTheme.copyWith(
+                    titleLarge: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: kColorScheme.onSecondaryContainer,
+                      fontSize: 16,
+                    ),
+                  )),
+          // themeMode: ThemeMode.system, // control the theme mode which is default
+          home: const Expenses(),
+        ),
+      );
+    },
   );
 }
