95ed559dd5c55198a90685052784814305517549
Sakina Mbatha <sakina@natix.io>
2023-09-11 10:27:04 +0200

    style: setting color schemes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+// color scheme - it's common for global variables to begin with 'k'
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 128, 227, 179),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 219, 127, 157),
-      ),
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimary,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
